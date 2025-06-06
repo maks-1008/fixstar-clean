c7cff18ad4d72aa05dbc02346bd0af113c7f899f
maks-1008 <maksim_pauk@outlook.com>
2025-06-06 17:49:19 +0300

    Fix: исправлен импорт bootstrap в main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap'
+// Bootstrap подключен через CDN в index.html
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
