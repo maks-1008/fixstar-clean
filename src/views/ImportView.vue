d067a1f2c10f066da19bc77af7032a76eeb49615
maks-1008 <maksim_pauk@outlook.com>
2025-06-06 14:32:33 +0300

    Заменил библиотеку xlsx на exceljs для безопасного импорта Excel-файлов

--- src/views/ImportView.vue
+++ src/views/ImportView.vue
@@ -63,7 +63,7 @@
 import { ref, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useProductStore } from '@/stores'
-import * as XLSX from 'xlsx'
+import ExcelJS from 'exceljs'
 
 export default {
   name: 'ImportView',
@@ -137,12 +137,23 @@ export default {
       return new Promise((resolve, reject) => {
         const reader = new FileReader()
 
-        reader.onload = (e) => {
+        reader.onload = async (e) => {
           try {
-            const data = new Uint8Array(e.target.result)
-            const workbook = XLSX.read(data, { type: 'array' })
-            const firstSheet = workbook.Sheets[workbook.SheetNames[0]]
-            const jsonData = XLSX.utils.sheet_to_json(firstSheet, { header: 1 })
+            const buffer = e.target.result
+            const workbook = new ExcelJS.Workbook()
+            await workbook.xlsx.load(buffer)
+
+            const worksheet = workbook.worksheets[0]
+            const jsonData = []
+
+            worksheet.eachRow({ includeEmpty: false }, (row) => {
+              const rowData = []
+              row.eachCell((cell) => {
+                rowData.push(cell.value)
+              })
+              jsonData.push(rowData)
+            })
+
             resolve(jsonData)
           } catch (error) {
             reject(error)
