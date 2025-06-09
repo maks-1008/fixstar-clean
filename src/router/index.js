caa5f091ab34c1552bde7fe5e6a29fd649813218
maks-1008 <maksim_pauk@outlook.com>
2025-06-09 07:38:47 +0300

    Fix: Изменен режим Vue Router на hash для совместимости с GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CatalogView from '../views/CatalogView.vue'
 import AboutView from '../views/AboutView.vue'
@@ -7,7 +7,7 @@ import ContactsView from '../views/ContactsView.vue'
 import CategoryView from '../views/CategoryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
